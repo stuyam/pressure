f5832abcc9f0cd23dd12f76f050967c963ce20ce
Stuart Yamartino <stuart@yamartino.com>
2015-11-08 02:25:14 -0500

    New API signature

--- src/pressure.js
+++ src/pressure.js
@@ -3,26 +3,34 @@
 // only the methods in this object can be called, making it the "public api"
 var Pressure = {
 
+  // targets any device with Force of 3D Touch
+  set: function(selector, closure){
+    Router.set(selector, closure);
+  },
+
+  // targets ONLY devices with Force Touch
+  setForceTouch: function(selector, closure){
+    Router.set(selector, closure, 'force');
+  },
+
+  // targets ONLY devices with 3D touch
+  set3DTouch: function(selector, closure){
+    Router.set(selector, closure, '3d');
+  },
 
   // this method is to determine if the browser and/or user have support for force touch of 3D touch
   // when this method is run, it will immediatly return if the browser does not support the force/3D touch,
   // however it will not return if the user has an supported trackpad or device until a click happens somewhere on the page
-  supported: function(closure, optionalType){
+  support: function(closure){
     Browser.checkSupport(closure);
   },
 
-  // targets any device with Force of 3D Touch
-  change: function(selector, closure, optionalType){
-    Router.changePressure(selector, closure, optionalType);
+  supportForceTouch: function(closure){
+    Browser.checkSupport(closure, 'force');
   },
 
-  // // targets ONLY devices with Force Touch
-  // changeForceTouch: function(selector, closure, optionalType){
-  //   Router.changePressure(selector, closure, 'force');
-  // },
+  support3DTouch: function(closure){
+    Browser.checkSupport(closure, '3d');
+  },
 
-  // // targets ONLY devices with 3D touch
-  // change3DTouch: function(selector, closure, optionalType){
-  //   Router.changePressure(selector, closure, '3d');
-  // }
 }
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ var Router = {
 
   // this method will return a force value from the user and will automatically determine if the user has Force or 3D Touch
   // it also accepts an optional type, this type is passed in by the following 2 methods to be explicit about which change event type they want
-  changePressure : function(selector, closure, type){
+  set : function(selector, closure, type){
     Event.build(selector, closure, function(){
       // Call ONLY the Force Touch method and only if the user supports it
       if(Support.type === 'force' && type === 'force'){
@@ -21,5 +21,16 @@ var Router = {
         Event.change3DTouch(selector, closure);
       }
     }.bind(this));
+  },
+
+  support : function(closure, type){
+    if(type === 'force'){
+
+    } else if(type === '3d'){
+
+    } else {
+      Browser.checkSupport(closure);
+    }
   }
+
 }
