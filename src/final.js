3e4f226c1593270ff85a084eaf945d07cef49b87
Stuart Yamartino <stuart@yamartino.com>
2015-11-12 17:16:19 -0500

    Removed Router & Final files and classes

--- src/final.js
+++
@@ -1,5 +0,0 @@
-// Check if the device is mobile or desktop
-Support.mobile = 'ontouchstart' in document;
-
-// Assign the Pressure object to the global object so it can be called from inside the self executing anonymous function
-window.Pressure = Pressure;
