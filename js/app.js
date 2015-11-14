6723c688e5a4bb07e4d15843d6c4fb919d79a33f
Stuart Yamartino <stuart@yamartino.com>
2015-11-13 21:00:22 -0500

    More more more

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function map(x, in_min, in_max, out_min, out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 var block = {
   start: function(){
   },
@@ -5,14 +9,14 @@ var block = {
   change: function(force, event){
     this.style.width = ((200 * force) + 200) + 'px';
     this.innerHTML = force;
-    this.style.backgroundColor = "rgb(" + parseInt(map(force, 0, 1, 255, 0)) + ",0," + parseInt(map(force, 0, 1, 0, 255)) +")";
+    this.style.backgroundColor = "rgb(" + parseInt(map(force, 0, 1, 255, 0)) + ",200," + parseInt(map(force, 0, 1, 0, 255)) +")";
     this.style.color = force > 0.4 ? 'white' : 'black';
   },
 
   end: function(){
     this.style.width = '200px';
     this.innerHTML = 0;
-    this.style.backgroundColor = 'red';
+    this.style.backgroundColor = '#FFC800';
     this.style.color = 'black';
   },
 
@@ -25,6 +29,17 @@ Pressure.set('#el1', block);
 Pressure.setForceTouch('#el2', block);
 Pressure.set3DTouch('#el3', block);
 
-function map(x, in_min, in_max, out_min, out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+
+Pressure.set('#peanuts', {
+  change: function(force, event){
+    this.style.webkitFilter = 'blur(' + map(force, 0, 0.7, 20, 0) + 'px)';
+  },
+
+  end: function(){
+    this.style.webkitFilter = 'blur(20px)';
+  },
+
+  unsupported: function(){
+    this.innerHTML = 'Your device / browser does not support this :(';
+  }
+});
