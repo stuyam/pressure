5125ed6ca6f4ebe6ff3083cc46c6f7e7cfbb1c1a
Stuart Yamartino <stuart@yamartino.com>
2016-01-16 00:41:08 -0500

    Move to app.js

--- js/app.js
+++ js/app.js
@@ -80,3 +80,30 @@ Pressure.setForceTouch('#element-force', {
     this.innerHTML = force + 'on a Mac';
   }
 });
+
+// Twitter BTN
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-72492481-1', 'auto');
+ga('send', 'pageview');
