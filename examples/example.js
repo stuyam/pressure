b28eb16f78681679e8932e7eba3a14b8575737d1
Stuart Yamartino <stuart@yamartino.com>
2016-01-30 18:38:59 -0500

    Removed old non relavent commented out examples

--- examples/example.js
+++ examples/example.js
@@ -1,23 +1,3 @@
-// // Get back a change value on all elements matching the selector
-// Pressure.change('#el1', function(force, event){
-//   console.log(force);
-//   // "this" is the element(s) returned by the closure
-//   this.style.width = (200 * force + 200) + 'px';
-//   // this.innerHTML = force;
-// });
-
-// // // Get back a change value on ONLY elements that support Force Touch (New Mac's)
-// // Pressure.changeForceTouch('#el2', function(force, event){
-// //   this.style.width = Math.max((200 * force), 200) + 'px';
-// //   this.innerHTML = force;
-// // });
-
-// // Get back a change value on ONLY elements that support 3D Touch (iPhone 6s & iPhone 6s Plus)
-// Pressure.change('#el3', function(force, event){
-//   this.style.width = (200 * force + 200) + 'px';
-//   this.innerHTML = force;
-// });
-
 // Example of change method with a failure closure
 // This structure can be used in any methods of Pressure
 // The failure block will return with an "error" and message showing why the device doesn't support 3D Touch and Force Touch
@@ -60,50 +40,8 @@ $('#el1-jquery').pressure(block);
 $('#el2-jquery').pressure(block, {only: 'force', css: false});
 $('#el3-jquery').pressure(block, {only: '3d'});
 
-$('h3').pressure({
-  start: function(){
-    console.log('preventDefault text');
-  }
-}, {preventDefault: false, css: false});
-
-// Pressure.set('#el1', {
+// $('h3').pressure({
 //   start: function(){
-//     console.log('started!');
-//   },
-
-//   change: function(force, event){
-//     this.style.width = ((200 * force) + 200) + 'px';
-//     this.innerHTML = force;
-//   },
-
-//   end: function(){
-//     console.log('ended!');
-//     this.style.width = '200px';
-//     this.innerHTML = 0;
-//   },
-
-//   unsupported: function(){
-//     console.log('UNSUPPORTED');
+//     console.log('preventDefault text');
 //   }
-// });
-
-// The "Supported" method is just a wrapper to determind if a browser / device supports 3D Touch or Force Touch
-// If the browser does not support them, then the "fail" will be called immediately
-// If the browser is supported but the device is not, a click somewhere on the page has to happen to determine success or failure
-// Pressure.support({
-//   success:function(){
-//     console.log('User and Browser both support force touch');
-//   },
-//   // if the browser does not support 3D or force touch this will be called as soon as it is loaded on the page
-//   failOnLoad: function(error){
-//     console.log('failInstant');
-//     console.log(error);
-//   },
-//   // if the browser is supported but the device is not (example: MacBook Air on El Capitan in Safari), this will be called whenever the first click is made anywhere on the page
-//   // this can't be called immediately on page load because it impossible to test the device support without testing a click on the page
-//   failOnClick: function(error){
-//     console.log('failLater');
-//     console.log(error);
-//   }
-
-// });
+// }, {preventDefault: false, css: false});
