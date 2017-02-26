21cfb4c21963c56cc4c2a815c2d038ac0b05074a
Stuart Yamartino <stuart@yamartino.com>
2017-02-26 13:20:48 -0500

    Wrong config

--- dist/pressure.js
+++ dist/pressure.js
@@ -491,7 +491,7 @@ var Config = {
   polyfillSpeedUp: 1000,
 
   // milliseconds it takes to go from 1 to 0 for the polyfill
-  polyfillSpeedDown: 5000,
+  polyfillSpeedDown: 0,
 
   // 'true' prevents the selecting of text and images via css properties
   preventSelect: true,
